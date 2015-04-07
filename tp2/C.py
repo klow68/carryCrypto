c87e2ee9427096ba9b950b820aa72ecfe751c666
Patrick Guimaraes <dragon_killer_68@hotmail.fr>
2015-04-07 16:59:42 +0200

    commit pour commit

---
+++ tp2/C.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+#-*-coding: utf-8 -*-
+
+import random
+import sys
+import math
+
+
+def miller_rabin(n,k = 10):
+	#n=2**s*d trouver s et d
+	s=?
+	d=?
+	for i in range(k):
+		r = random.randint(2,n-2)
+		x = a**d%n
+		if x==1 or x==-1:
+			for r in range(1,s-1):
+
+
